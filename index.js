1902ff2d4488a6601e1ad75f6217439dedb2d6f9
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-01-27 15:59:41 -0500

    Pass unit tests for getLastCarInfo

--- index.js
+++ index.js
@@ -166,8 +166,9 @@ function getCarInfoByIndex(inventory, index) {
  * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
  * it will return `This is a Lincoln Town Car`.
 */
-function getLastCarInfo(/* code here */) {
-  /* code here */
+function getLastCarInfo(inventory) {
+  const carId = inventory[inventory.length - 1];
+  return `This is a ${carId.car_make} ${carId.car_model}`;
 }
 
 /**
